57d126f1438d6f79ec85afa4bee1adec1d5d2c5f
vincentbatten27 <vbatten123@gmail.com>
2026-02-20 09:52:44 -0500

    ..

--- Multi_Factor_Fall_25/streamlit_optimizer.py
+++ Multi_Factor_Fall_25/streamlit_optimizer.py
@@ -3,7 +3,7 @@ import pandas as pd
 from RunSim_utils import *
 
 st.title("Multi-Factor Investing")
-st.caption("Optimize portfolio weights to achieve target three fama-french exposures from S&P 500 universe.")
+st.caption("Optimize portfolio weights to achieve target three fama-french exposures from S&P 500 equity universe.")
 
 # =============================================================================
 # INPUT 1: Total Portfolio Value
