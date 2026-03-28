3fd934591e1d2f5474614e33de0f17a83956c708
vincentbatten27 <vbatten123@gmail.com>
2026-03-28 17:07:11 -0400

    .............

--- Multi_Factor_Fall_25/streamlit_optimizer.py
+++ Multi_Factor_Fall_25/streamlit_optimizer.py
@@ -637,7 +637,7 @@ footer = """
 <div class="footer">
     <p>Developed by <b>Vincent Batten</b> | 
     <a href="mailto:vincentbatten27@gmail.com">Contact Me</a> | 
-    Built with <b>Johanan Pranesh | 
+    Built with <b>Johanan Pranesh <b><br>
     Created alongside <b>Nate Songstad, Kshitij Bhandari</b><br>
     Sponsored by: <b>Jordan Weintraub</b></p>
 </div>
