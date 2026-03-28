a6286635997426d90ac8b483b54a1b3d5c6f151c
vincentbatten27 <vbatten123@gmail.com>
2026-03-28 16:17:55 -0400

    .....

--- Multi_Factor_Fall_25/streamlit_optimizer.py
+++ Multi_Factor_Fall_25/streamlit_optimizer.py
@@ -126,7 +126,8 @@ def get_betas():
     weights_opt_d = curr_weights - relativedelta(days=1)
 
     for obj in OBJECTIVES:
-        path = f"Front_End_Strategies/{obj}/rebal_explored_{obj}_active_{weights_opt_d.strftime('%Y-%m-%d')}.csv"
+        base_path = os.path.dirname(os.path.abspath(__file__))
+        path = os.path.join(base_path,f"Front_End_Strategies/{obj}/rebal_explored_{obj}_active_{weights_opt_d.strftime('%Y-%m-%d')}.csv")
         df = pd.read_csv(path)
         curr_df = df.sort_values(by="reward")
         betaA = curr_df.iloc[-1][0]
