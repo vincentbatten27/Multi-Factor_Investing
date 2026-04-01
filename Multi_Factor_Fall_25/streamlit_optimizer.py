1c17bc45f5b9fa679237c8e7b71163b282fff0b6
vincentbatten27 <vbatten123@gmail.com>
2026-04-01 09:13:48 -0400

    Update streamlit_optimizer.py

--- Multi_Factor_Fall_25/streamlit_optimizer.py
+++ Multi_Factor_Fall_25/streamlit_optimizer.py
@@ -120,9 +120,12 @@ st.divider()
 def get_betas():
     OBJECTIVES = ["max_return", "sharpe", "sortino", "volatility", "downside_vol"]
     betas = {}
-
     today = pd.Timestamp.now()
-    target_date = today.normalize()
+    if today.day >= 2:
+        target_date = today.replace(day=1)
+    else:
+        target_date = (today - pd.DateOffset(months=1)).replace(day=1)
+    target_date = target_date.normalize()
     curr_weights = target_date.date()
     weights_opt_d = curr_weights - relativedelta(days=1)
 
