f9bae77fc17853f1b034b8f2c5d54ad4dbca9372
vincentbatten27 <vbatten123@gmail.com>
2026-04-02 17:34:26 -0400

    Update RunSim_utils.py

--- Multi_Factor_Fall_25/RunSim_utils.py
+++ Multi_Factor_Fall_25/RunSim_utils.py
@@ -1790,9 +1790,10 @@ def monte_carlo_simulation(n_simulations,mbetaA,mbetaB,mbetaC,type, in_years1, o
                     obj_key
                 )
             elif rebal_freq == 'pre_drm':
-                oos1_new_performance = get_premade_mrebalance_front_end(
-                out_years, obj_key,n_simulations
+                oos1_list, oos1_avg, oos1_y, expected_betas, rebalance_opt_weights = get_premade_mrebalance_front_end(
+                out_years, obj_key, n_simulations
                 )
+                return oos1_list, oos1_y, oos1_avg, expected_betas, rebalance_opt_weights
         globals()[f'x{i}_df']=oos1_new_performance.copy()
         globals()[f'y{i}_df']=new_performances.copy()
         results.append(globals()[f'x{i}_df'])
