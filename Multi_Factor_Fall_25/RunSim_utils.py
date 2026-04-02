098a5d5c0631ef40e702f63396b1295015cc3fe5
vincentbatten27 <vbatten123@gmail.com>
2026-04-02 17:21:15 -0400

    Update RunSim_utils.py

--- Multi_Factor_Fall_25/RunSim_utils.py
+++ Multi_Factor_Fall_25/RunSim_utils.py
@@ -1811,9 +1811,7 @@ def get_premade_mrebalance_front_end(os_years, obj,num_iteration):
     results = []
 
     for i in range(num_iteration):
-        df = pd.read_csv(
-            fSCRIPT_DIR
-            / "Front_End_Strategies_Iteration_3\pre_drm_2025_{obj}_run{i+1}.csv",
+        df = pd.read_csv(fSCRIPT_DIR / "Front_End_Strategies_Iteration_3\pre_drm_2025_{obj}_run{i+1}.csv",
             index_col=0,
             parse_dates=True,
         )
