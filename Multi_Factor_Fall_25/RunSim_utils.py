9593bfad0eb151f274159ce989ce0facfb84023d
vincentbatten27 <vbatten123@gmail.com>
2026-05-05 12:56:48 -0400

    Update RunSim_utils.py

--- Multi_Factor_Fall_25/RunSim_utils.py
+++ Multi_Factor_Fall_25/RunSim_utils.py
@@ -1315,7 +1315,7 @@ def new_run_with_backtest_mrebalance_front_end(
     start_date = noos1_new_performance.index[0]
     end_date = noos1_new_performance.index[-1]
 
-    return noos1_new_performance
+    return noos1_new_performance, expected_betas, None
 
 
 # In[118]:
@@ -1775,7 +1775,7 @@ def monte_carlo_simulation(n_simulations,mbetaA,mbetaB,mbetaC,type, in_years1, o
     global oos1_new_performance1
     for i in range(n_simulations):
         if rebal_freq == 'drm' or rebal_freq == 'm':
-            oos1_new_performance = new_run_with_backtest_mrebalance_front_end(
+            oos1_new_performance, expected_betas, rebalance_opt_weights = new_run_with_backtest_mrebalance_front_end(
                 type,
                 in_years1,
                 out_years,
@@ -1787,6 +1787,7 @@ def monte_carlo_simulation(n_simulations,mbetaA,mbetaB,mbetaC,type, in_years1, o
                 c_portf,
                 obj_key
             )
+            return oos1_new_performance, None, None expected_betas, rebalance_opt_weights
         elif rebal_freq == 'pre_drm':
             oos1_list, oos1_avg, oos1_y, expected_betas, rebalance_opt_weights = get_premade_mrebalance_front_end(
             out_years, obj_key, n_simulations
