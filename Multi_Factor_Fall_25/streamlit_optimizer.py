aae5313daf80f948f961a57c166d22c19370ec9b
vincentbatten27 <vbatten123@gmail.com>
2026-05-05 08:39:26 -0400

    Update streamlit_optimizer.py

--- Multi_Factor_Fall_25/streamlit_optimizer.py
+++ Multi_Factor_Fall_25/streamlit_optimizer.py
@@ -161,7 +161,7 @@ def get_betas():
         base_path = os.path.dirname(os.path.abspath(__file__))
         path = os.path.join(
             base_path,
-            f"Front_End_Strategies_Iteration_4_excess{obj}/rebal_explored_{obj}_{weights_opt_d.strftime('%Y-%m-%d')}.csv",
+            f"Front_End_Strategies_Iteration_4c_excess_surrogate_{obj}/rebal_explored_{obj}_{weights_opt_d.strftime('%Y-%m-%d')}.csv",
         )
         st.write(f"DEBUG: {path}")  # temporary, remove after
         df = pd.read_csv(path)
