b183138ce8f99c3ad80bfdff3081e05121db1411
vincentbatten27 <vbatten123@gmail.com>
2026-05-05 12:32:14 -0400

    Update streamlit_optimizer.py

--- Multi_Factor_Fall_25/streamlit_optimizer.py
+++ Multi_Factor_Fall_25/streamlit_optimizer.py
@@ -634,6 +634,7 @@ if st.button("Retrieve Weights", type="primary", width="stretch"):
             with st.spinner("Running Monte Carlo simulation..."):
                 curr_weights = target_date.date()
                 end_sim = curr_weights - relativedelta(days=1)
+                st.write(f'{obj_key}')
                 oos1_list, oos1_avg, oos1_y, expected_betas, rebalance_opt_weights = (
                     monte_carlo_simulation(
                         num_runs,
